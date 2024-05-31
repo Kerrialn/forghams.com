b91a268bf9bd210557f78cf4f92d6aeafc82e2ab
kez <kerrialbeckettnewham@gmail.com>
2024-05-31 09:39:10 +0200

    fix swup intergration

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\Swup\SwupBundle::class => ['all' => true],
+    Symfony\UX\LazyImage\LazyImageBundle::class => ['all' => true],
 ];
