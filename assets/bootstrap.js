f3fa2d7f4b0a6bf8287133b9b0dfada2fcbcb836
kez <kerrialbeckettnewham@gmail.com>
2024-08-01 13:15:02 +0200

    fix image layout loading

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -2,5 +2,6 @@ import { startStimulusApp } from '@symfony/stimulus-bundle';
 import ImageGrid from 'stimulus-image-grid'
 
 const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-app.register('image-grid', ImageGrid)
+document.addEventListener('DOMContentLoaded', function() {
+    app.register('image-grid', ImageGrid)
+})
